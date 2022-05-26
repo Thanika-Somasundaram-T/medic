eab781fed31e0af73e482cf72c8f22fa930b6464
Thanika_Somasundaram_T <thanikasomasundaram1801@gmail.com>
2022-05-26 14:22:42 +0530

    Finally added marker.

--- src/components/maps/maps.js
+++ src/components/maps/maps.js
@@ -1,22 +1,23 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import './maps.css';
 import { GoogleMap, Marker } from '@react-google-maps/api';
 import { useDataLayerValue } from '../../dataLayer';
 
 const Maps = () => {
+	const [map, setMap] = useState('');
 	const [ state ] = useDataLayerValue();
 	const center = { lat: state.latitude, lng: state.longitude };
 	return (
 			<div className='map-root'>
-				<div className='map'>
 					<GoogleMap
 						zoom={ 14 }
 						center={ center }
 						mapContainerClassName='map'
+						options={''}
+						onLoad={(map) => setTimeout(() => setMap(map)) }
 					>
-						<Marker position={ center }/>
+						{ map && <Marker position={ center }/> }
 					</GoogleMap>
-				</div>
 			</div>
 	);
 }
